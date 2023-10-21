88fdc67899aa5cc25ec94d740e48992604de750d
bertintshisuaka2 <bertintshisuaka@hotmail.com>
2023-10-20 21:43:46 -0400

    all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 // Access element using id
-var articlesDiv = document.getElementById('articles');
-var headerDiv = document.getElementById('header');
+//var articlesDiv = document.getElementById('articles');
+////var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[0].style.fontSize = '50px';
+//articlesDiv.children[0].style.fontSize = '50px';
+//headerDiv.children[0].style.color = 'white';
+
+
+<!-------------------->
+
+//Access element using id
+
+var articlesDiv = document.getElementsByClassId('articles');
+var headerDiv = document.getElementsById('header');
+
+// Change style by accessing style object's properties
+articlesDiv.children[0].style.fontSize ='50px';
 headerDiv.children[0].style.color = 'white';
-
-
